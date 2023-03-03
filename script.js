ca9c11395b2508283ebe725fb870673ea0cb7961
Théo Sinatti <sinatti.theo@gmail.com>
2023-03-03 19:44:46 +0100

    First function calling the API

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 console.log("Foo bar");
 
 
+async function recupererSuperFilms() {
+	//Récupération des pièces eventuellement stockées dans le localStorage
+	let films = window.localStorage.getItem("films");
+
+	if (films === null) {
+		// Récupération des pièces depuis l'API
+		const reponse = await fetch("http://127.0.0.1:8000/api/v1/titles/?imdb_score_min=9.2");
+		films = await reponse.json();
+		// Transformation des pièces en JSON
+		const valeurFilms = JSON.stringify(films);
+		// Stockage des informations dans le localStorage
+		window.localStorage.setItem("films", valeurFilms);
+	} else {
+		films = JSON.parse(films);
+	}
+	for (let film of films.results){
+		console.log(film.title)
+	}
+}
+
+
 //gestion des bouttons 
 const boutonCool = document.querySelector(".btn-cool");
 
-boutonCool.addEventListener("click", function () {
-	console.log("Héhéhé")
-});
+boutonCool.addEventListener("click", recupererSuperFilms);
 
 
