ea394651a8f68ed32debfd179123a9b8e9cb1f9e
Théo Sinatti <sinatti.theo@gmail.com>
2023-03-03 16:37:25 +0100

    Created a few base files (html, css, js). Actually copied them from previous projects, so as of now it contains a lot of code to remote/change

---
+++ script.js
@@ -0,0 +1,12 @@
+// Actions au chargement de la page ?
+console.log("Foo bar");
+
+
+//gestion des bouttons 
+const boutonCool = document.querySelector(".btn-cool");
+
+boutonCool.addEventListener("click", function () {
+	console.log("Héhéhé")
+});
+
+
