95b4697b206400c7a758e299967b430e9bf87c32
Théo Sinatti <sinatti.theo@gmail.com>
2023-03-21 17:13:08 +0100

    Commented and cleaned up main script

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 import {getBestRatedMovies} from "./jsmodules/api_requests.js";
 import { createFrontPageMovie, createSection} from "./jsmodules/page_layout.js";
 
+// WEBPAGE MAIN SCRIPTS : EVENT LISTENERS SETTING & FUNCTION CALLS ON PAGE LOADING
 
-// Appel de fonctions au chargement de la page
+// FIRST STEP : Getting all the best rated movies for each genre and creating the caroussels.
+
+// Get the 8 best rated movies across all genres from the API
 getBestRatedMovies().then(function(data){
+	// Create the front page movie banner with the first one (the best rated movie)
 	createFrontPageMovie(data.shift());
+	// Populate the first carrousel with the 7 following movies (the 7 best rated movies across all genres, after the front page one)
 	createSection(data, "Best");
 });
+// Get the 7 best rated action movies from the API
 getBestRatedMovies("Action").then(function(data){
+	// Populate the 2nd carrousel with these movies
 	createSection(data, "Action");
 });
+// Get the 7 best rated comedies from the API
 getBestRatedMovies("Comedy").then(function(data){
+	// Populate the 3rd carrousel with these movies
 	createSection(data, "Comedy");
 });
+// Get the 7 best rated Sci-Fi movies from the API
 getBestRatedMovies("Sci-Fi").then(function(data){
+	// Populate the 4th carrousel with these movies
 	createSection(data, "Sci-Fi");
 });
 
+// SECOND STEP : Adding the event listener on the caroussels left and right buttons.
 
-
-// Add listener to left and right scroll buttons for caroussels
-var mesBtn = document.querySelectorAll(".btn");
-for (let btn of mesBtn){
-	btn.addEventListener("click", function(event) {
+var arrowBtns = document.querySelectorAll(".btn");
+// For each button
+for (let btn of arrowBtns){
+	// Create an event listener on click
+	btn.addEventListener("click", function() {
+		// Retrieve all the needed information (carrousel to move, direction) from the button name.
 		let btnName = this.id;
 		let genre = btnName.split("__")[0];
 		let direction = btnName.split("__")[2].split("_")[0];
@@ -41,6 +54,5 @@ for (let btn of mesBtn){
 				behavior: 'smooth'
 			});
 		}
-
-	})
+	});
 }
