f0f65b31f35c38c782fdc8e047773bb9c9470c39
Théo Sinatti <sinatti.theo@gmail.com>
2023-03-21 17:39:30 +0100

    Comment and clean up of page_layout.js script. Also, had not been consistent with the way to spell carousel. Corrected it in all scripts.

--- jsmodules/page_layout.js
+++ jsmodules/page_layout.js
@@ -1,77 +1,79 @@
 import { getMovieDetails, fetchMovieDescription} from "./api_requests.js";
 import {populateModalWindow} from "./modal_window.js";
 
+// From a movie's information (from the API) passed as arg, this function create the "front page movie" section of the page
 export function createFrontPageMovie(movie) {
+	// Send a request to the API to retrieve the movie description and add it to the corresponding div
 	fetchMovieDescription(movie);
-	var sectionImg = document.querySelector(".frontPageMovie__leftBlock");
-	var sectionTitle = document.querySelector(".frontPageMovie__rightBlock__line1");
-	var sectionBtn = document.querySelector(".frontPageMovie__rightBlock__line2");
-	const nomElement = document.createElement("h1");
+	// Retrieving all the container div from the HTML
+	let sectionImg = document.querySelector(".frontPageMovie__leftBlock");
+	let sectionTitle = document.querySelector(".frontPageMovie__rightBlock__line1");
+	let sectionBtn = document.querySelector(".frontPageMovie__rightBlock__line2");
+	// Creating the title div
+	let nomElement = document.createElement("h1");
 	nomElement.innerText = movie.title;
-	const imageElement = document.createElement("img");
+	// Creating img div
+	let imageElement = document.createElement("img");
 	imageElement.src = movie.image_url;
 	imageElement.className = "frontPageMovie__leftBlock__img"
-	const seeMoreButton = document.createElement("button");
+	// Creating the button
+	let seeMoreButton = document.createElement("button");
 	seeMoreButton.className = "frontPageMovie__rightBlock__line2__btn"
 	seeMoreButton.dataset.id = movie.id;
 	seeMoreButton.textContent = "Plus d'infos";
+	// Setting an event listener on click : opens the modal window for the film
 	seeMoreButton.addEventListener("click", function(event){
 		getMovieDetails(event).then(function(movieData){
 			displayOverlayAndModal(movieData);
 		})
 	});
-
-	// On rattache la balise film a la section
+	// Appends all the created element to the HTML container elements
 	sectionImg.appendChild(imageElement);
 	sectionTitle.appendChild(nomElement);
 	sectionBtn.appendChild(seeMoreButton);
 
 }
 
+// From a list of movies' information (from the API) passed as arg, this function create a carousel section
 export function createSection(filmList, genre){
+	// Getting the carousel concerned from the HTML and placing the title
 	if (genre === null){
-		var section = document.querySelector("#Best__caroussel");
-		var sectionTitle = document.createElement("p");
+		// If no genre is passed to the function, it means the carousel concerned is the "best movies across all category" one
+		var section = document.querySelector("#Best__carousel");
+		let sectionTitle = document.createElement("p");
 		sectionTitle.innerText = "Meilleurs films (toutes catégories)";
 	}
 	else {
-		var section = document.querySelector("#" + genre + "__caroussel");
-		var sectionTitle = document.createElement("p");
+		// If a genre is passed, it designates which carousel is concerned and the title to give the section
+		var section = document.querySelector("#" + genre + "__carousel");
+		let sectionTitle = document.createElement("p");
 		sectionTitle.innerText = genre;
 	}
-	
-	// section.appendChild(sectionTitle);
+	// Now the bulk of the function : for each film, place it in the carousel
 	for (let film of filmList){
-		const filmElement = document.createElement("div");
+		// Create a div
+		let filmElement = document.createElement("div");
 		filmElement.className = "movieMiniature";
-		
-		// const nomElement = document.createElement("span");
-		// nomElement.innerText = film.title;
-		// const noteElement = document.createElement("p");
-		// noteElement.innerText = film.imdb_score ?? "(Pas de note IMDB)";
-		const imageElement = document.createElement("img");
-		// imageElement.className = "item-image";
+		// Create an img from the movie img URL
+		let imageElement = document.createElement("img");
 		imageElement.src = film.image_url;
 		imageElement.dataset.id = film.id;
-		// addListenerShowDetails(imageElement)
+		// Adds the click event listener on the image : opens the modal window for the movie
 		imageElement.addEventListener("click", function(event){
 			getMovieDetails(event).then(function(movieData){
 				displayOverlayAndModal(movieData);
 			})
 		});
-		
-		
-		// On rattache la balise film a la section
-		
+		// Add the newly created div to the carousel
 		section.appendChild(filmElement);
-		// filmElement.appendChild(nomElement);
-		// filmElement.appendChild(noteElement);
+		// Add the img to the div
 		filmElement.appendChild(imageElement);
-		// filmElement.appendChild(nomElement);
 	}
 
 }
 
+// Handle the display of the modal window and of the overlay behind it.
+// The creation on the content of the window is handled by populateModalWindow, which this function calls.
 function displayOverlayAndModal(movie) {
 	//Calling the function to populate the modal window with the movie information
 	populateModalWindow(movie);
@@ -83,10 +85,23 @@ function displayOverlayAndModal(movie) {
 	btnClose.addEventListener("click", removeOverlayAndModal);
 }
 
+// Handle the emptying of the modal window content and the removing of it and the overlay
 function removeOverlayAndModal() {
+	// Removing the overlay
 	let overlay = document.querySelector(".overlay");
-	let window = document.querySelector(".infoWindow");
-	//TODO : change to empty all containers within the window and not the window itself
-	window.innerHTML = "<div class='infoWindow__closebtndiv'><button class='infoWindow__closebtndiv__btn'>+</button></div><div class='infoWindow__title'><div class='infoWindow__title__line1'></div><div class='infoWindow__title__line2'></div></div><div class='infoWindow__informations'><div class='infoWindow__informations__line1'></div><div class='infoWindow__informations__line2'></div></div><div class='infoWindow__details'><div class='infoWindow__details__leftBlock'></div><div class='infoWindow__details__rightBlock'></div></div>";
 	overlay.style.display = "none";
+	// Emptying the modal window content divs
+	let titleLine1 = document.querySelector(".infoWindow__title__line1");
+	titleLine1.innerHTML = "";
+	let titleLine2 = document.querySelector(".infoWindow__title__line2");
+	titleLine2.innerHTML = "";
+	let infoLine1 = document.querySelector(".infoWindow__informations__line1");
+	infoLine1.innerHTML = "";
+	let infoLine2 = document.querySelector(".infoWindow__informations__line2");
+	infoLine2.innerHTML = "";
+	let detailsLeftBlock = document.querySelector(".infoWindow__details__leftBlock");
+	detailsLeftBlock.innerHTML = "";
+	let detailsRightBlock = document.querySelector(".infoWindow__details__rightBlock");
+	detailsRightBlock.innerHTML = "";
+	// window.innerHTML = "<div class='infoWindow__closebtndiv'><button class='infoWindow__closebtndiv__btn'>+</button></div><div class='infoWindow__title'><div class='infoWindow__title__line1'></div><div class='infoWindow__title__line2'></div></div><div class='infoWindow__informations'><div class='infoWindow__informations__line1'></div><div class='infoWindow__informations__line2'></div></div><div class='infoWindow__details'><div class='infoWindow__details__leftBlock'></div><div class='infoWindow__details__rightBlock'></div></div>";
 }
--- script.js
+++ script.js
@@ -3,43 +3,43 @@ import { createFrontPageMovie, createSection} from "./jsmodules/page_layout.js";
 
 // WEBPAGE MAIN SCRIPTS : EVENT LISTENERS SETTING & FUNCTION CALLS ON PAGE LOADING
 
-// FIRST STEP : Getting all the best rated movies for each genre and creating the caroussels.
+// FIRST STEP : Getting all the best rated movies for each genre and creating the carousels.
 
 // Get the 8 best rated movies across all genres from the API
 getBestRatedMovies().then(function(data){
 	// Create the front page movie banner with the first one (the best rated movie)
 	createFrontPageMovie(data.shift());
-	// Populate the first carrousel with the 7 following movies (the 7 best rated movies across all genres, after the front page one)
+	// Populate the first carousel with the 7 following movies (the 7 best rated movies across all genres, after the front page one)
 	createSection(data, "Best");
 });
 // Get the 7 best rated action movies from the API
 getBestRatedMovies("Action").then(function(data){
-	// Populate the 2nd carrousel with these movies
+	// Populate the 2nd carousel with these movies
 	createSection(data, "Action");
 });
 // Get the 7 best rated comedies from the API
 getBestRatedMovies("Comedy").then(function(data){
-	// Populate the 3rd carrousel with these movies
+	// Populate the 3rd carousel with these movies
 	createSection(data, "Comedy");
 });
 // Get the 7 best rated Sci-Fi movies from the API
 getBestRatedMovies("Sci-Fi").then(function(data){
-	// Populate the 4th carrousel with these movies
+	// Populate the 4th carousel with these movies
 	createSection(data, "Sci-Fi");
 });
 
-// SECOND STEP : Adding the event listener on the caroussels left and right buttons.
+// SECOND STEP : Adding the event listener on the carousels left and right buttons.
 
 var arrowBtns = document.querySelectorAll(".btn");
 // For each button
 for (let btn of arrowBtns){
 	// Create an event listener on click
 	btn.addEventListener("click", function() {
-		// Retrieve all the needed information (carrousel to move, direction) from the button name.
+		// Retrieve all the needed information (carousel to move, direction) from the button name.
 		let btnName = this.id;
 		let genre = btnName.split("__")[0];
 		let direction = btnName.split("__")[2].split("_")[0];
-		var scrollArea = document.querySelector("#" + genre + "__caroussel__scroll");
+		var scrollArea = document.querySelector("#" + genre + "__carousel__scroll");
 		if (direction === "right"){
 			scrollArea.scroll({
 				top: 0,
